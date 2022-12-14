b1aef76532da8c1f7b31640fa0f5a1fdf67f1852
SebastianJimenez2510 <sebastian.jimenezgarro@gmail.com>
2022-12-14 00:14:41 +0000

    Fecht finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,44 +21,16 @@ function Home() {
 /* Lista de Tareas */
 export function ToDosList(){
 	const [tareas, setTareas] = useState([]);
-	const [usuario, setUsuario] = useState("");
+	
 
-	const BASE_URL = "https://assets.breatheco.de/apis/fake/todos/";
+	const BASE_URL = "http://assets.breatheco.de/apis/fake/todos/";
 
-	const crearUsuario = async (e) => {
-	  e.preventDefault();
-	  let data = new FormData(e.target);
-	  let username = data.get("username");
-	  console.log("la variable username es: ", username);
-	  setUsuario(username);
-  
-	  let URI = `${BASE_URL}user/${username}`;
-  
-	  try {
-		let respuesta = await fetch(URI, {
-		  method: "POST",
-		  headers: { "Content-Type": "application/json" },
-		  body: JSON.stringify([]),
-		});
-		console.log(respuesta);
-		if (respuesta.ok) {
-		  alert("Usuario creado correctamente");
-		  return;
-		} else {
-		  alert("Error al crear usuario, o el usuario ya existe");
-		  setUsuario(username);
-		}
-		let respuestaJSON = await respuesta.json();
-		console.log(respuestaJSON);
-	  } catch {
-		(e) => console.log(e);
-	  }
-	};
-
+	
 	const traerListaTareas = async () => {
-		let URI = `${BASE_URL}user/${usuario}`;
+		let URI = `${BASE_URL}`;
+		console.log("Traer Tareas ", URI)
 		try {
-		  let respuesta = await fetch(URI);
+		   fetch(URI);
 		  if (respuesta.ok) {
 			let respuestaJSON = await respuesta.json();
 			console.log(respuestaJSON);
@@ -83,27 +55,26 @@ export function ToDosList(){
 		);
 		setTareas(todosActualizados);
 
-		let URI = `${BASE_URL}user/${usuario}`;
+		let URI = `${BASE_URL}`;
 
-		let respuesta = await fetch(URI, {
+		fetch (URI, {
 			method: "PUT",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(todosActualizados),  
-		});
-
-		if (respuesta.ok) {
+		}).then(Response => { if (Response.ok) {
 			alert("Se agrego exitosamente la tarea!")
 
 			traerListaTareas();
 		} else {
 			alert("Error al agregar la tarea!")
-		}
+		}});
+		
+	
 	
 	};
 
 	const eliminarTarea = async id => {
-		let URI = `${BASE_URL}user/${usuario}`;
-
+		let URI = `${BASE_URL}`;
 		const todosActualizados =  tareas.filter((item, index) => {
 			return index !== id;
 		  });
@@ -111,7 +82,7 @@ export function ToDosList(){
 
 		if (todosActualizados.length > 0){
 			try {
-				let respuesta = await fecth (URI, {
+				fetch (URI, {
 					method: "PUT",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify(todosActualizados), 
@@ -127,7 +98,7 @@ export function ToDosList(){
 				(e) => console.log(e); 
 			}
 		} else {
-			let respuesta = await fetch(URI, {
+			 fetch(URI, {
 				method: "DELETE",
 				headers: { "Content-Type": "application/json" },
 			});
@@ -137,7 +108,7 @@ export function ToDosList(){
 
 	useEffect(() => {
 		traerListaTareas()
-	}, [usuario]); 
+	}, []); 
 
 	
 
@@ -153,17 +124,6 @@ export function ToDosList(){
 	};
     return(
 		<>
-			<div className="card">
-      		<form
-        		onSubmit={(e) => {
-         		crearUsuario(e);
-        		}}
-      		>
-        		<input type="text" placeholder="Nombre de usuario" name="username" />
-        		<button type="submit">Crear Usuario</button>
-      			</form>
-	  		</div>
-		
 			<TareaFormulario onSubmit={agregarTodos} />
 			<div className="todos-list-container">
 				{tareas.map((tarea, indice) =>
